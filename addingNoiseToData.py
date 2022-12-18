157f30b72162e9622b2073e12f69a2d1a07fec6a
Josef <JosefSo@users.noreply.github.com>
2022-12-18 15:15:58 +0200

    Added addingNoiseToData.py

---
+++ addingNoiseToData.py
@@ -0,0 +1,41 @@
+import numpy as np
+import soundfile as sf
+from scipy.signal import resample
+
+
+def add_noise(voice_data, noise_data, noise_level):
+    # Convert the voice data and noise data to numpy arrays
+    voice_data = np.array(voice_data, dtype=np.float32)
+    noise_data = np.array(noise_data, dtype=np.float32)
+
+    # Normalize the noise data
+    noise_data = noise_data / np.max(np.abs(noise_data))
+
+    # Resample the noise data to match the sample rate of the voice data
+    print(f"int(voice_data.shape[0]) {(voice_data.shape[0])}")
+    print(f"noise_data.shape[0]) {noise_data.shape[0]}")
+    print(f"int(voice_data.shape[0] / noise_data.shape[0]) {int(voice_data.shape[0] / noise_data.shape[0])}" )
+    noise_data = resample(noise_data, 1)
+
+    # Trim the noise data to match the length of the voice data
+    noise_data = noise_data[:voice_data.shape[0]]
+
+    # Mix the noise and voice data, with the specified noise level
+    mixed_data = voice_data + noise_level * noise_data
+
+    return mixed_data
+
+
+# Load the voice data and noise data
+voice_data, _ = sf.read('voice.wav')
+noise_data, _ = sf.read('noise.wav')
+
+# Add noise to the voice data at various levels
+mixed_data_1 = add_noise(voice_data, noise_data, 0.1)
+mixed_data_2 = add_noise(voice_data, noise_data, 0.5)
+mixed_data_3 = add_noise(voice_data, noise_data, 0.9)
+
+# Save the mixed data to new audio files
+sf.write('mixed_data_1.wav', mixed_data_1, 44100)
+sf.write('mixed_data_2.wav', mixed_data_2, 44100)
+sf.write('mixed_data_3.wav', mixed_data_3, 44100)
--- common_voice_ru.py
+++ common_voice_ru.py
@@ -1,5 +1,6 @@
 from datasets import load_dataset
+import soundfile
 
 dataset = load_dataset("common_voice", "ru")
 
-dataset.save_to_disk("/Users/joseph/Desktop/myDataCommonVoice")
+dataset.save_to_disk("/Users/user/Desktop/VoiceData")
